d38f261ac3dbe64f10ed8a812332e78ac046591a
Shamil <shamilvkage@gmail.com>
2023-08-07 16:08:17 +0530

    new updates

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("correction");
+import express, { Application, Request, Response } from "express";
+
+class App {
+    public app: Application
+
+    constructor () {
+        this.app = express();
+        this.routes();
+    }
+
+    protected routes(): void {
+        this.app?.route("/").get((req: Request, res: Response) => {
+            res.send('Welcome');
+        });
+    }
+}
+
+
+const port:number = 8000
+const app = new App().app
+
+app.listen(port,()=>{
+    console.log("Server has been started!")
+});
